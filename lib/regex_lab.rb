e2ab56075077bf634e749a79f71c707a4c85a4fd
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-02 03:22:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def starts_with_a_vowel?(word)
 
-  if word.match(/\b+[aeiou]/i)
+  if word.match(/\b[aeiou]/i)
     true
   else
     false
@@ -17,11 +17,10 @@ def words_starting_with_un_and_ending_with_ing(text)
   else
     false
   end
-
 end
 
 def words_five_letters_long(text)
-
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
